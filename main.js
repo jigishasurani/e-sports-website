d6396d9de67a1a798aa97696b72d6ce6b01c157d
Jigisha surani <139609284+jigishasurani@users.noreply.github.com>
2023-07-17 21:26:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let navbar=document.querySelector(".navbar");
 menu.addEventListener("click",function(){
     navbar.classList.toggle("active");
 });
+
 window.onscroll =()=>{
     navbar.classList.remove("active");
 }
