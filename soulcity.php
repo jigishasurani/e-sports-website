33a5111977ba9875e6008e14da998eaab58c358d
Jigisha surani <139609284+jigishasurani@users.noreply.github.com>
2023-07-17 21:34:04 +0530

    Update soulcity.php

--- soulcity.php
+++ soulcity.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body>
+    
     <?php include("mnav.php")?>
         
     <section id="page-header" class="about-header">
